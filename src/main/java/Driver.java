05e9da098c205b40d5091cf65880c6007de78b3d
Muhammad Edgar Nadhif <118090975+edgarnadhif@users.noreply.github.com>
2025-06-03 02:28:18 -0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,15 +6,15 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Countq :" + counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Countw :" + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Count:" + counter.getCount());
+		System.out.println("Count3 :" + counter.getCount());
 		
 	}
 
